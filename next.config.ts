dc75c8579b0fffe03a2ec79b3fcf72e5cea98c3a
Wassim-dxdydz <abahriwassim44@gmail.com>
2026-06-22 20:45:00 +0200

    feat

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
-  },
-  turbopack: {
-    root: process.env.VERCEL ? "/vercel/path0" : "C:\\Users\\abahr\\Downloads\\ML-WebApp\\frontend",
   },
   experimental: {
     optimizePackageImports: ["framer-motion", "lucide-react", "next-themes"],
